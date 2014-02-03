bd0366ae17296f8cac768b10497a512b37948692
Jasper van Rijbroek <jasper199069@live.nl>
2014-02-03 16:30:17 +0100

    Updated install.sql

--- administrator/components/com_translations/install/install.sql
+++ administrator/components/com_translations/install/install.sql
@@ -3,9 +3,11 @@
 -- ----------------------------
 CREATE TABLE `#__translations_translations` (
   `translations_translation_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `iso_code` varchar(255) NOT NULL,
   `row` bigint(20) NOT NULL,
   `table` varchar(255) NOT NULL,
   `original` varchar(20) NOT NULL,
+  `translated` varchar(255) NOT NULL,
   `created_on` datetime NOT NULL,
   `created_by` int(11) NOT NULL,
   `modified_on` datetime NOT NULL,
@@ -13,21 +15,4 @@ CREATE TABLE `#__translations_translations` (
   `locked_on` datetime NOT NULL,
   `locked_by` int(11) NOT NULL,
   PRIMARY KEY (`translations_translation_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
---  Table structure for `#__translations_translations_relations`
--- ----------------------------
-CREATE TABLE `#__translations_translations_relations` (
-  `translations_translations_relation_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `translations_translation_id` bigint(20) unsigned NOT NULL,
-  `lang` varchar(255) NOT NULL,
-  `translated` tinyint(1) NOT NULL,
-  `created_on` datetime NOT NULL,
-  `created_by` int(11) NOT NULL,
-  `modified_on` datetime NOT NULL,
-  `modified_by` int(11) NOT NULL,
-  `locked_on` datetime NOT NULL,
-  `locked_by` int(11) NOT NULL,
-  PRIMARY KEY (`translations_translations_relation_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
