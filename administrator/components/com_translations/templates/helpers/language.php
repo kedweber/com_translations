59612824bbe534ba10970114fb158c6db0bf3782
Jasper van Rijbroek <jasper199069@live.nl>
2014-04-03 15:52:46 +0200

    Added fix for constant view=article.

--- administrator/components/com_translations/templates/helpers/language.php
+++ administrator/components/com_translations/templates/helpers/language.php
@@ -13,27 +13,28 @@ class ComTranslationsTemplateHelperLanguage extends KTemplateHelperAbstract
 		// First for our knowledge we get the original language (if exists.)
 		$original = $this->_getOriginalLanguage($config->row, $config->table);
 		$html = '<style src="media://com_translations/css/translations.css" />';
+        $view = KInflector::singularize(KRequest::get('get.view', 'string'));
 
 		foreach($this->_getLanguages() as $language)
 		{
 			$relation = $this->_getLanguage($config, $language->lang_code);
 
 			if($language->lang_code == $original->iso_code) {
-				$html .= ' <a href="' . $this->getTemplate()->getView()->createRoute('view=article&id=' . $config->row . '&backendlanguage=' . $language->lang_code) . '"><div class="badge badge-info">' . strtoupper(substr($language->lang_code, 0, 2)) . '</a></div>';
+				$html .= ' <a href="' . $this->getTemplate()->getView()->createRoute('view=' . $view . '&id=' . $config->row . '&backendlanguage=' . $language->lang_code) . '"><div class="badge badge-info">' . strtoupper(substr($language->lang_code, 0, 2)) . '</a></div>';
 			}
 			else if($relation->translated)
 			{
-				$html .= ' <a href="' . $this->getTemplate()->getView()->createRoute('view=article&id=' . $config->row . '&backendlanguage=' . $language->lang_code) . '"><div class="badge badge-success">' . strtoupper(substr($language->lang_code, 0, 2)) . '</a></div>';
+				$html .= ' <a href="' . $this->getTemplate()->getView()->createRoute('view=' . $view . '&id=' . $config->row . '&backendlanguage=' . $language->lang_code) . '"><div class="badge badge-success">' . strtoupper(substr($language->lang_code, 0, 2)) . '</a></div>';
 			}
 			else
 			{
 				if(strtotime('+ 2 weeks', strtotime($original->created_on)) > strtotime(date('d-m-Y H:i:s')))
 				{
-					$html .= ' <a href="' . $this->getTemplate()->getView()->createRoute('view=article&id=' . $config->row . '&backendlanguage=' . $language->lang_code) . '"><div class="badge badge-warning">' . strtoupper(substr($language->lang_code, 0, 2)) . '</a></div>';
+					$html .= ' <a href="' . $this->getTemplate()->getView()->createRoute('view=' . $view . '&id=' . $config->row . '&backendlanguage=' . $language->lang_code) . '"><div class="badge badge-warning">' . strtoupper(substr($language->lang_code, 0, 2)) . '</a></div>';
 				}
 				else if(strtotime('+ 2 weeks', strtotime($original->created_on)) < strtotime(date('d-m-Y H:i:s')))
 				{
-					$html .= ' <a href="' . $this->getTemplate()->getView()->createRoute('view=article&id=' . $config->row . '&backendlanguage=' . $language->lang_code) . '"><div class="badge badge-important">' . strtoupper(substr($language->lang_code, 0, 2)) . '</a></div>';
+					$html .= ' <a href="' . $this->getTemplate()->getView()->createRoute('view=' . $view . '&id=' . $config->row . '&backendlanguage=' . $language->lang_code) . '"><div class="badge badge-important">' . strtoupper(substr($language->lang_code, 0, 2)) . '</a></div>';
 				}
 			}
 		}
